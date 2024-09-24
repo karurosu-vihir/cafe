e342c28133d13f33cc3af98719cb85fa5e414c54
dry <karurosu.vihir@gmail.com>
2024-09-24 07:51:50 -0600

    init

---
+++ db.js
@@ -0,0 +1,112 @@
+export default function info() {
+  return {
+    "producto": [
+      {
+        "nombre": "Espresso",
+        "precio":60,
+        "descripcion": "Café fuerte y concentrado, perfecto para un golpe de energía.",
+        "tipo": "cafe",
+        "comprados":70
+      },
+      {
+        "nombre": "Café Americano",
+        "precio":50,
+        "descripcion": "Café diluido con agua caliente, más suave que el espresso.",
+        "tipo": "cafe",
+        "comprados":100
+      },
+      {
+        "nombre": "Café Latte",
+        "precio":80,
+        "descripcion": "Café espresso con abundante leche al vapor.",
+        "tipo": "cafe",
+        "comprados":50
+      },
+      {
+        "nombre": "Cappuccino",
+        "precio":60,
+        "descripcion": "Café espresso con leche al vapor y espuma.",
+        "tipo": "cafe",
+        "comprados":120
+      },
+      {
+        "nombre": "Café Mocha",
+        "precio":85,
+        "descripcion": "Mezcla de café, chocolate y leche, con crema batida opcional.",
+        "tipo": "cafe",
+        "comprados":15
+      },
+      {
+        "nombre": "Frappuccino de Vainilla",
+        "precio":90,
+        "descripcion": "Bebida fría de café con sabor a vainilla y crema batida.",
+        "tipo": "bebida helada",
+        "comprados":45
+      },
+      {
+        "nombre": "Té Helado de Limón",
+        "precio":60,
+        "descripcion": "Té negro frío con un toque de limón.",
+        "tipo": "bebida helada",
+        "comprados":5
+      },
+      {
+        "nombre": "Smoothie de Mango",
+        "precio":80,
+        "descripcion": "Batido refrescante hecho con mango natural.",
+        "tipo": "bebida helada",
+        "comprados":70
+      },
+      {
+        "nombre": "Limonada Frozen",
+        "precio":70,
+        "descripcion": "Limonada fresca con hielo triturado para un toque helado.",
+        "tipo": "bebida helada",
+        "comprados":90
+      },
+      {
+        "nombre": "Café Frappé",
+        "precio":84,
+        "descripcion": "Café frío con hielo, perfecto para el verano.",
+        "tipo": "bebida helada",
+        "comprados":30
+      },
+      {
+        "nombre": "Croissant de Jamón y Queso",
+        "precio":100,
+        "descripcion": "Croissant relleno de jamón y queso, ideal para el desayuno o almuerzo.",
+        "tipo": "postre",
+        "comprados":80
+      },
+      {
+        "nombre": "Pastel de Zanahoria",
+        "precio":80,
+        "descripcion": "Dulce y suave pastel con sabor a zanahoria y cobertura de queso crema.",
+        "tipo": "postre",
+        "comprados":10
+      },
+      {
+        "nombre": "Tostada Francesa",
+        "precio":96,
+        "descripcion": "Pan bañado en huevo, frito y servido con miel o frutas.",
+        "tipo": "postre",
+        "comprados":40
+      },
+      {
+        "nombre": "Ensalada César",
+        "precio":130,
+        "descripcion": "Ensalada clásica con lechuga, crutones, queso parmesano y aderezo César.",
+        "tipo": "postre",
+        "comprados":15
+      },
+      {
+        "nombre": "Sandwich de Pollo",
+        "precio":120,
+        "descripcion": "Sandwich con pechuga de pollo, lechuga, tomate y mayonesa.",
+        "tipo": "postre",
+        "comprados":50
+      }
+    ]
+  }
+}
+  
---
+++ index.js
@@ -0,0 +1,99 @@
+import info from "./db.js"
+
+const data = info()
+const productos = data.producto
+
+document.addEventListener("DOMContentLoaded",()=>{
+    orgnizarproductos(window.location.pathname)
+})
+
+const orgnizarproductos = (pathname) => {
+    let venta = ""
+    switch (pathname) {
+        case "/pages/Cafes.html":
+            venta = productos.filter(producto => producto.tipo === "cafe")
+            console.log(venta)
+            init('menus',venta)
+            break;
+        case "/pages/helado.html":
+            venta = productos.filter(producto => producto.tipo === "bebida helada")
+            console.log(venta);
+            init('menus',venta)
+            break;
+        case "/pages/postre.html":
+            venta = productos.filter(producto => producto.tipo === "postre")
+            console.log(venta)
+            init('menus',venta)
+            break;
+            case "/":
+                console.log(productospopulares());
+                break;
+        default:
+            break;
+    }
+} 
+
+const productospopulares = () => {
+   let poulares = [...productos].sort((a,b)=> b.comprados - a.comprados).slice(0,5);
+   return poulares;
+}
+
+const init = (clase, products) => {
+ let main_div = document.querySelector(`.${clase}`);
+ products.map((product)=>{
+    let div = creacioncajas(product);
+    main_div.appendChild(div)
+ })
+}
+
+// Creacion de cajas
+const creacioncajas = (producto) => {
+    const div = document.createElement('div');
+    div.className = 'menu';
+    div.innerHTML = `
+    <div class="menu_info">
+        <div class="menu_info_titulo">
+            <h3>${producto.nombre}</h3>
+            <p>L. ${producto.precio}.00</p>
+        </div>
+        <p class="desc">${producto.descripcion}</p>
+    </div>
+    <img src="../img/taza-de-cafe.png" alt="">
+    `
+    return div
+}
+
+
+// Funciones click
+const toggle_ham = ()=>{
+    const ul = document.querySelector('nav ul');
+    if(ul.className === "navbar-ham" ){
+        ul.classList.remove('navbar-ham');
+        ul.classList.add('navbar');
+    }
+    else{
+    ul.classList.add('navbar-ham');
+    ul.classList.remove('navbar');
+    }
+}
+
+const toggle_dropdown = ()=>{
+    const menu = document.querySelectorAll('.navbar-item a')[1];
+    const ul = document.querySelector('.dropdown');
+    if(ul.style.display  === "" ){
+        ul.style.display = "block";
+        menu.style.backgroundColor = "#591F0B";
+        menu.style.color = "white";
+    }
+    else{
+        const responsive = document.querySelector('nav ul');
+        if(responsive.className !== "navbar-ham" ){
+            menu.style.backgroundColor = "#9E7418";
+            menu.style.color = "black";
+        }
+        ul.style.display = "";
+    }
+}
+
+window.toggle_dropdown = toggle_dropdown
+window.toggle_ham = toggle_ham
